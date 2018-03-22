2107d19a3211bc7ed2774f604a736377c79deb95
ischonger <ischonger@gmail.com>
2018-03-22 16:16:53 +0100

    compute cov-matrices without build-ins

---
+++ scripts/covMatrix_self.R
@@ -0,0 +1,15 @@
+#acm <- vcov(model)
+#mv <- c(1:14) # vector of the mean values of the columns of sC
+#sC.matrix <- matrix(unlist(sC), ncol = 16, byrow = FALSE) # sC as a matrix
+#sC.matrix <- sC.matrix[,-16] <- sC.matrix[,-8]
+#for(col in 1:14) {
+#  mv[col] <- mean(sC.matrix[,col])
+#}
+#mv
+# create means for each column
+#sC_mean <- matrix(data=1, nrow=nrow(sC.matrix)) %*% t(matrix(mv)) 
+# creates a difference matrix sCd: sC_difference
+#sC.matrix <- sC.matrix[,-1]
+#sCd <- sC.matrix - sC_mean
+#creates the covariance matrix
+#sC.cov <- (nrow(sC.matrix)-1)^-1 %*% t(sCd) %*% sCd
